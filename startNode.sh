4808caea7fec9600b6b538340ad1949aaf93e6e7
shinhua0425 <shinhua0425@gmail.com>
2017-05-12 18:21:38 +0800

    123

--- startNode.sh
+++ startNode.sh
@@ -3,15 +3,9 @@
 # Setting Env andStrat Node 
 echo "&&& Start startNode.sh ...<<<"
 APP_ROOT_HOME=`pwd`
-export PATH=${PATH}:/u01/nodejs/lib/node_modules/npm/bin/node-gyp-bin:/u01/app/node_modules/.bin:/u01/nodejs/lib/node_modules/.bin:/u01/nodejs/bin
-echo "PATH:${PATH}"
-cd ../lib/node_modules/npm/
-mkdir javaDayNodejsDemo
-cp -R ${APP_ROOT_HOME}/* javaDayNodejsDemo
-cd javaDayNodejsDemo
 chmod 777 *
-ls -a
-node -v
-#node ./bin/www
+cp -R node_modules/* /u01/nodejs/lib/node_modules/
+export PATH=${PATH}:/u01/nodejs/lib/node_modules/npm/bin/node-gyp-bin:/u01/app/node_modules/.bin:/u01/nodejs/lib/node_modules/.bin:/u01/nodejs/bin
+node ./bin/www
 echo "end"
 
