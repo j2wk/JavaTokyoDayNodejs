de999c86698b3aee6397203c1db260f0bfd0d600
shinhua0425 <shinhua0425@gmail.com>
2017-05-12 19:03:49 +0800

    up

--- startNode.sh
+++ startNode.sh
@@ -5,12 +5,14 @@ echo "&&& Start startNode.sh ...<<<"
 APP_ROOT_HOME=`pwd`
 chmod 777 *
 cp -R node_modules/* /u01/nodejs/lib/node_modules/
-export PATH=${PATH}:/u01/nodejs/lib/node_modules/npm/bin/node-gyp-bin:/u01/app/node_modules/.bin:/u01/nodejs/lib/node_modules/.bin:/u01/nodejs/bin
+export PATH=${PATH}:/u01/apps/:/u01/nodejs/lib/node_modules/npm/bin/node-gyp-bin:/u01/app/node_modules/.bin:/u01/nodejs/lib/node_modules/.bin:/u01/nodejs/bin
 cd /u01/nodejs/lib/node_modules/npm
 mkdir javaDayNodejsDemo
 cp -R /u01/apps/* /u01/nodejs/lib/node_modules/npm/javaDayNodejsDemo
 cd /u01/nodejs/bin
 console.log(process.mainModule.paths)
-#node /u01/nodejs/lib/node_modules/npm/javaDayNodejsDemo/bin/www
+console.log(${process.mainModule.paths})
+cd  ${APP_ROOT_HOME}
+node ./bin/www
 echo "end"
 
