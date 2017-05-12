01bec701a76436e70257b03ea07f916f3578ec40
shinhua0425 <shinhua0425@gmail.com>
2017-05-12 17:26:45 +0800

    121

--- startNode.sh
+++ startNode.sh
@@ -3,8 +3,9 @@
 # Setting Env andStrat Node 
 echo "&&& Start startNode.sh ...<<<"
 export PATH=${PATH}:/u01/nodejs/lib/node_modules/npm/bin/node-gyp-bin:/u01/app//node_modules/.bin:/u01/nodejs/lib/node_modules/.bin:/u01/nodejs/bin
-echo ${PATH}
-echo "run"
-node /u01/app/bin/www
+echo "PATH:${PATH}"
+cd ../lib/node_modules/npm/
+ls -a
+#node /u01/app/bin/www
 echo "end"
 
