d275f650d7dfff9f8209da5cf939fb5f06eff834
shinhua0425 <shinhua0425@gmail.com>
2017-05-12 17:16:04 +0800

    node /u01/app/bin/www

--- startNode.sh
+++ startNode.sh
@@ -5,6 +5,6 @@ echo "&&& Start startNode.sh ...<<<"
 export PATH=${PATH}:/u01/nodejs/lib/node_modules/npm/bin/node-gyp-bin:/u01/app//node_modules/.bin:/u01/nodejs/lib/node_modules/.bin:/u01/nodejs/bin
 echo ${PATH}
 echo "run"
-start
+node /u01/app/bin/www
 echo "end"
 
