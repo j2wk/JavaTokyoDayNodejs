b42915054106c536cf4d8436fabecf709e85224b
shinhua0425 <shinhua0425@gmail.com>
2017-05-12 16:01:50 +0800

    up2

--- startNode.sh
+++ startNode.sh
@@ -2,9 +2,9 @@
 
 # Setting Env andStrat Node 
 echo "&&& Start startNode.sh ...<<<"
-export PATH=${PATH}:../nodejs/bin:../nodejs/lib/node_modules:./bin/www
+export PATH=${PATH}:../lib/node_modules/npm/bin/node-gyp-bin:./node_modules/.bin:../nodejs/bin:../nodejs/lib/node_modules:../lib/node_modules/.bin:./bin/www:
 echo ${PATH}
 echo "run"
-node ./bin/www
+#node ./bin/www
 echo "end"
 
