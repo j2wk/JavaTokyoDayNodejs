4f002cb9177fdc4cf6150e0ac52866b56b80a8e5
shinhua0425 <shinhua0425@gmail.com>
2017-05-12 19:12:25 +0800

    FinalUpdate

--- startNode.sh
+++ startNode.sh
@@ -5,13 +5,15 @@ echo "&&& Start startNode.sh ...<<<"
 APP_ROOT_HOME=`pwd`
 chmod 777 *
 cp -R node_modules/* /u01/nodejs/lib/node_modules/
-export PATH=${PATH}:/u01/apps/:/u01/nodejs/lib/node_modules/npm/bin/node-gyp-bin:/u01/app/node_modules/.bin:/u01/nodejs/lib/node_modules/.bin:/u01/nodejs/bin
+cd bin
+cp * /u01/nodejs/bin
 cd /u01/nodejs/lib/node_modules/npm
 mkdir javaDayNodejsDemo
+export PATH=${PATH}:/u01/apps/:/u01/lib/node_modules/npm/javaDayNodejsDemo:/u01/nodejs/lib/node_modules/npm/bin/node-gyp-bin:/u01/app/node_modules/.bin:/u01/nodejs/lib/node_modules/.bin:/u01/nodejs/bin
 cp -R /u01/apps/* /u01/nodejs/lib/node_modules/npm/javaDayNodejsDemo
 cd /u01/nodejs/bin
-console.log(process.mainModule.paths)
-console.log(${process.mainModule.paths})
+#console.log(process.mainModule.paths)
+#console.log(${process.mainModule.paths})
 cd  ${APP_ROOT_HOME}
 node ./bin/www
 echo "end"
