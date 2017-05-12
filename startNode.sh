3afaff7e10551755f3449d5c2aa84495f37a4542
shinhua0425 <shinhua0425@gmail.com>
2017-05-12 13:08:38 +0800

    2131

--- startNode.sh
+++ startNode.sh
@@ -2,12 +2,14 @@
 
 # Setting Env andStrat Node 
 echo "&&& Start startNode.sh ...<<<"
-echo "ppp"
-pwd
+##echo "ppp"
+##echo pwd
 echo $NODE_ENV
 echo "1"
 echo ${NODE_ENV}
 echo "2"
+cd bin
+ls -a
 #echo "node++"
 #node ./bin/www
 echo "end"
