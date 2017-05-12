3235c6ea8bf3cd943f5f24b422cc3978a49bc246
shinhua0425 <shinhua0425@gmail.com>
2017-05-12 18:57:27 +0800

    123

--- startNode.sh
+++ startNode.sh
@@ -6,6 +6,11 @@ APP_ROOT_HOME=`pwd`
 chmod 777 *
 cp -R node_modules/* /u01/nodejs/lib/node_modules/
 export PATH=${PATH}:/u01/nodejs/lib/node_modules/npm/bin/node-gyp-bin:/u01/app/node_modules/.bin:/u01/nodejs/lib/node_modules/.bin:/u01/nodejs/bin
-node ./bin/www
+cd /u01/nodejs/lib/node_modules/npm
+mkdir javaDayNodejsDemo
+cp -R /u01/apps/* /u01/nodejs/lib/node_modules/npm/javaDayNodejsDemo
+cd /u01/nodejs/bin
+console.log(process.mainModule.paths)
+#node /u01/nodejs/lib/node_modules/npm/javaDayNodejsDemo/bin/www
 echo "end"
 
